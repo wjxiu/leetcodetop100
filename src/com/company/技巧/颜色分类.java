370a60ada8e0e96fc8167c827f6b2b77c9e8d9f3
wjxiu <2277346638@qq.com>
2023-07-09 11:07:32 +0800

    颜色分类 双指针遍历

--- src/com/company/技巧/颜色分类.java
+++ src/com/company/技巧/颜色分类.java
@@ -12,24 +12,31 @@ public class 颜色分类 {
         new 颜色分类().sortColors(ints);
         System.out.println(Arrays.toString(ints));
     }
-//    单指针遍历
+//    双指针遍历
     public void sortColors(int[] nums) {
-        int head=0;
+        int p0=0;
+        int p1=0;
         for (int i = 0; i <nums.length; i++) {
-            if (nums[i]==0){
-                int temp=nums[head];
-                nums[head]=nums[i];
-                nums[i]=temp;
-                head++;
-            }
-        }
-        for (int i = head; i <nums.length; i++) {
             if (nums[i]==1){
-                int temp=nums[head];
-                nums[head]=nums[i];
+                int temp=nums[p1];
+                nums[p1]=nums[i];
                 nums[i]=temp;
-                head++;
+                p1++;
+            }else if(nums[i]==0){
+                int temp=nums[p0];
+                nums[p0]=nums[i];
+                nums[i]=temp;
+//                防止p0指针移动到   数字1的位置
+//                把交换后的nums[i]的换到p1，
+                if (p1>p0){
+                     temp=nums[p1];
+                    nums[p1]=nums[i];
+                    nums[i]=temp;
+                }
+                p1++;
+                p0++;
             }
         }
+
     }
 }
